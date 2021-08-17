e567cfffc566fc819dd3cac73745a9e33e48c795
beanz <vegamehra@gmail.com>
2021-08-17 18:27:13 -0400

    Add project files.

---
+++ BeanbotSharp/Commands/Ping.cs
@@ -0,0 +1,22 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Threading.Tasks;
+
+namespace BeanbotSharp.Commands
+{
+    public class Ping : BaseCommandModule
+    {
+        [
+            Command("ping"),
+            Description("Displays the bot's current ping"),
+            Cooldown(3, 5, CooldownBucketType.User),
+            RequireBotPermissions(DSharpPlus.Permissions.SendMessages)
+        ]
+        public async Task PingCommand(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+            await ctx.RespondAsync(String.Format(":ping_pong: pong! my ping is {0}ms", ctx.Client.Ping));
+        }
+    }
+}
---
+++ BeanbotSharp/Program.cs
@@ -0,0 +1,61 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.CommandsNext.Exceptions;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace BeanbotSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync()
+        {
+            // Discord client
+            var discord = new DiscordShardedClient(new DiscordConfiguration()
+            {
+                Token = Environment.GetEnvironmentVariable("DISCORD_TOKEN"),
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged
+            });
+
+            // CommandsNext
+            var commands = await discord.UseCommandsNextAsync(new CommandsNextConfiguration()
+            {
+                StringPrefixes = new[] { "~" }
+            });
+
+            // Register commands
+            commands.RegisterCommands(Assembly.GetExecutingAssembly());
+
+            // Register command error handler
+            foreach (CommandsNextExtension command in commands.Values)
+            {
+                command.CommandErrored += CmdErroredHandler;
+            }
+
+            await discord.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private static async Task CmdErroredHandler(CommandsNextExtension _, CommandErrorEventArgs e)
+        {
+            var failedChecks = ((ChecksFailedException)e.Exception).FailedChecks;
+            foreach (var failedCheck in failedChecks)
+            {
+                if (failedCheck is CooldownAttribute attribute)
+                    await e.Context.RespondAsync($"you're executing this command too fast! please wait {Math.Ceiling(attribute.GetRemainingCooldown(e.Context).TotalSeconds)}s before trying again :/");
+                else if (failedCheck is RequireBotPermissionsAttribute)
+                    await e.Context.RespondAsync($"i dont have the proper permissions to do that >.<");
+                else if (failedCheck is RequireUserPermissionsAttribute)
+                    await e.Context.RespondAsync($"sorry, you arent allowed to do that :(");
+            }
+        }
+    }
+}
